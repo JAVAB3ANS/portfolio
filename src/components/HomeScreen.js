a6b564fea44d2bc20d56b3b63c76869850f755f3
Jason Vu <jvu@scu.edu>
2022-08-22 15:04:38 -0700

    Update HomeScreen.js

--- src/components/HomeScreen.js
+++ src/components/HomeScreen.js
@@ -7,7 +7,7 @@ function HomeScreen() {
         <div id="lead">
             <div id="lead-content">
                 <h1>Jason Vu</h1>
-                <h2>Aspiring Software Developer</h2><a aria-label="resume" draggable="false" href="https://javab3ans.github.io/javab3ans/resume.html" target="_blank" rel="noopener noreferrer" className="btn-rounded-white"><FontAwesomeIcon icon={faCopy}/> My Résumé!</a>
+                <h2>Aspiring Software Developer</h2><a aria-label="resume" draggable="false" href="https://javab3ans.github.io/portfolio/resume.html" target="_blank" rel="noopener noreferrer" className="btn-rounded-white"><FontAwesomeIcon icon={faCopy}/> My Résumé!</a>
             </div>
             <div id="lead-overlay"></div>
             <div id="lead-down"> 
